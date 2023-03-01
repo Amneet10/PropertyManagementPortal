bcc7fe50df1057467c3377d272b0d35103e9e6ca
Amneet Kaur <kauramneet986@hotmail.com>
2023-03-01 16:00:21 -0500

    Implemented Sorting on Building Model

--- PropertyManagementPortal/Pages/Buildings/Index.cshtml.cs
+++ PropertyManagementPortal/Pages/Buildings/Index.cshtml.cs
@@ -20,15 +20,49 @@ namespace PropertyManagementPortal.Pages.Buildings
         {
             _context = context;
         }
+        public string OwnerName { get; set; }
+        public string TypeOfBuilding { get; set; }
+        public string AddressOfBuilding { get; set; }
+        
 
-        public IList<Building> Building { get;set; } = default!;
+        public IList<Building> Buildings { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            if (_context.Building != null)
+            OwnerName = sortOrder == "OwnerName" ? "OwnerName_desc" : "OwnerName";
+            TypeOfBuilding = sortOrder == "TypeOfBuilding"? "TypeOfBuilding_desc" : "TypeOfBuilding";
+            AddressOfBuilding = sortOrder == "AddressOfBuilding" ? "AddressOfBuilding_desc" : "AddressOfBuilding";
+
+
+            IQueryable<Building> BuildingsIQ = from s in _context.Building
+                                             select s;
+
+            switch (sortOrder)
             {
-                Building = await _context.Building.ToListAsync();
+                case "OwnerName":
+                    BuildingsIQ = BuildingsIQ.OrderBy(s => s.OwnerName);
+                    break;
+                case "OwnerName_desc":
+                    BuildingsIQ = BuildingsIQ.OrderByDescending(s => s.OwnerName);
+                    break;
+                case "TypeOfBuilding":
+                    BuildingsIQ = BuildingsIQ.OrderBy(s => s.TypeOfBuilding);
+                    break;
+                case "TypeOfBuilding_desc":
+                    BuildingsIQ = BuildingsIQ.OrderByDescending(s => s.TypeOfBuilding);
+                    break;
+                case "AddressOfBuilding":
+                    BuildingsIQ = BuildingsIQ.OrderBy(s => s.AddressOfBuilding);
+                    break;
+                case "AddressOfBuilding_desc":
+                    BuildingsIQ = BuildingsIQ.OrderByDescending(s => s.AddressOfBuilding);
+                    break;
+                default:
+                    BuildingsIQ = BuildingsIQ.OrderBy(s => s.Id);
+                    break;
             }
+            
+            Buildings = await BuildingsIQ.AsNoTracking().ToListAsync();
         }
 
        
