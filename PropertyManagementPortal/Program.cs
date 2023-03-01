c5bde62af0a251bc2658fce68a13186a00c0840c
Amneet Kaur <kauramneet986@hotmail.com>
2023-03-01 12:42:18 -0500

    Implemented Identity

--- PropertyManagementPortal/Program.cs
+++ PropertyManagementPortal/Program.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PropertyManagementPortal.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<PropertyManagementPortalContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PropertyManagementPortalContext") ?? throw new InvalidOperationException("Connection string 'PropertyManagementPortalContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PropertyManagementPortalContext>();
 
 var app = builder.Build();
 
